c487627d1bb38d6f2292192cb0e0daaea5a38550
JubeirMorad <jubiermorad@gmail.com>
2026-03-23 13:35:36 +0200

    Add entity (user)

---
+++ Projects-Management-Studio.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+
+
+namespace Projects_Management_Studio.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string Username { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string PasswordHash { get; set; } = null!;
+
+        public string Role { get; set; } = "User";
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
