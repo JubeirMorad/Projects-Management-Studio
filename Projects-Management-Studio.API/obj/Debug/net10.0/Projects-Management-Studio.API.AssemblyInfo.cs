c1b984e9cdf68897081eb28ff9c469efc4aef8a9
JubeirMorad <jubiermorad@gmail.com>
2026-03-24 20:15:10 +0300

    test : verify register endpoint works correctly

--- Projects-Management-Studio.API/obj/Debug/net10.0/Projects-Management-Studio.API.AssemblyInfo.cs
+++ Projects-Management-Studio.API/obj/Debug/net10.0/Projects-Management-Studio.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projects-Management-Studio.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf858e272fbc665e8d348d7326b77cbdea83f3a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a7718dde98ef098f5fa8db0a1aa47f29db80e4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projects-Management-Studio.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projects-Management-Studio.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Projects-Management-Studio.App/obj/Debug/net10.0/Projects-Management-Studio.App.AssemblyInfo.cs
+++ Projects-Management-Studio.App/obj/Debug/net10.0/Projects-Management-Studio.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projects-Management-Studio.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3660ac5200e8f87bc482a3908be097e50b14a0bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a7718dde98ef098f5fa8db0a1aa47f29db80e4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projects-Management-Studio.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projects-Management-Studio.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
