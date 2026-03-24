0992c7c827cb29d267250f6d4fd25f897aeabe3d
JubeirMorad <jubiermorad@gmail.com>
2026-03-24 21:28:58 +0300

    add method to Auth service interface

--- Projects-Management-Studio.App/Interfaces/Services/IAuthService.cs
+++ Projects-Management-Studio.App/Interfaces/Services/IAuthService.cs
@@ -7,6 +7,8 @@ namespace Projects_Management_Studio.App.Services.Interfaces
 {
     public interface IAuthService
     {
-        Task RegisterAsync(string userName , string email, string password);
+        Task RegisterAsync(string userName, string email, string password);
+
+        Task<(string token, string refreshToken)> LoginAsync(string email, string password); 
     }
 }
