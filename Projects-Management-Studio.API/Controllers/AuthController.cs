6a7718dde98ef098f5fa8db0a1aa47f29db80e4d
JubeirMorad <jubiermorad@gmail.com>
2026-03-24 20:03:32 +0300

    implement register endpoint

--- Projects-Management-Studio.API/Controllers/AuthController.cs
+++ Projects-Management-Studio.API/Controllers/AuthController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Projects_Management_Studio.API.Contracts;
 using Projects_Management_Studio.App.Services.Interfaces;
+using Projects_Management_Studio.Domain.Entities;
 
 namespace Projects_Management_Studio.API.Controllers
 {
@@ -16,5 +18,16 @@ namespace Projects_Management_Studio.API.Controllers
         {
             this.authService = authService;
         }
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequest request)
+        {
+            await authService.RegisterAsync(request.Name, request.Email, request.Password);
+
+            return Ok();
+        }
+
+
     }
 }
