51897dce194c0fee57369c142274efa65401d559
JubeirMorad <jubiermorad@gmail.com>
2026-03-28 20:03:20 +0300

    add task repository with methods (AddAsync, GetTasksByUserIdAsync)

---
+++ Projects-Management-Studio.App/Interfaces/Repositories/ITaskRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Projects_Management_Studio.Domain.Entities;
+
+namespace Projects_Management_Studio.App.Interfaces.Repositories
+{
+    public interface ITaskRepository
+    {
+        Task AddAsync(TaskItem taskItem);
+        Task<List<TaskItem>?> GetTasksByUserIdAsync(Guid userId);
+    }
+}
---
+++ Projects-Management-Studio.Infra/Repostories/TaskRepository.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Projects_Management_Studio.App.Interfaces.Repositories;
+using Projects_Management_Studio.Domain.Entities;
+using Projects_Management_Studio.Infra.Data;
+
+namespace Projects_Management_Studio.Infra.Repostories
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly AppDbContext _context;
+
+        public TaskRepository(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+        public async Task AddAsync(TaskItem taskItem)
+        {
+            await _context.Tasks.AddAsync(taskItem);
+        }
+
+        public async Task<List<TaskItem>?> GetTasksByUserIdAsync(Guid userId)
+        {
+            return await _context.Tasks
+                        .Where(t => t.AssignedToUserId == userId)
+                        .ToListAsync();
+        }
+    }
+}
