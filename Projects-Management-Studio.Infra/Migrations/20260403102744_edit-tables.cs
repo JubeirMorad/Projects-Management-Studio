977437a963264e369c731f20112fcebe23b5844a
JubeirMorad <jubiermorad@gmail.com>
2026-04-03 13:20:46 +0300

    entities: add fields to (project & user)

---
+++ Projects-Management-Studio.Infra/Migrations/20260403102744_edit-tables.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projects_Management_Studio.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class edittables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Users_OwnerId",
+                table: "Projects");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_AssignedToUserId",
+                table: "Tasks");
+
+            migrationBuilder.CreateTable(
+                name: "ProjectMembers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProjectId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectMembers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProjectMembers_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProjectMembers_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectMembers_ProjectId",
+                table: "ProjectMembers",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectMembers_UserId",
+                table: "ProjectMembers",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Users_OwnerId",
+                table: "Projects",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_AssignedToUserId",
+                table: "Tasks",
+                column: "AssignedToUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Users_OwnerId",
+                table: "Projects");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_AssignedToUserId",
+                table: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "ProjectMembers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Users_OwnerId",
+                table: "Projects",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_AssignedToUserId",
+                table: "Tasks",
+                column: "AssignedToUserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
