c71dd1398578b5a37a211c8062343c43d2ac0774
JubeirMorad <jubiermorad@gmail.com>
2026-05-10 11:12:10 +0300

    feat: test (Assign task) -> task service

---
+++ Tests/Services/TaskServiceTest.cs
@@ -0,0 +1,78 @@
+
+
+using Moq;
+using Projects_Management_Studio.App.Interfaces.Repositories;
+using Projects_Management_Studio.App.Services;
+using Projects_Management_Studio.Domain.Entities;
+
+namespace Tests.Services
+{
+    public class TaskServiceTest
+    {
+        [Fact]
+        public async Task AssignTaskAsync_OwnerCanAssignTask()
+        {
+            //
+            //
+            // Arrange 
+            var userRepo = new Mock<IUserRepository>();
+            var projectRepo = new Mock<IProjectRepository>();
+            var taskRepo = new Mock<ITaskRepository>();
+            var memberRepo = new Mock<IMemberRepository>();
+            var unitOfWork = new Mock<IUnitOfWork>();
+
+            Guid taskId = Guid.NewGuid();
+            Guid userId = Guid.NewGuid();
+            Guid projectId = Guid.NewGuid();
+            Guid currentUserId = Guid.NewGuid();
+
+
+            TaskItem task = new()
+            {
+                Id = taskId,
+                Title = "task 1",
+                Description = null,
+                AssignedToUserId = null,
+                ProjectId = projectId
+            };
+
+            Project project = new()
+            {
+                Id = projectId,
+                Name = "Project 1",
+                Description = null,
+                OwnerId = currentUserId
+            };
+
+            //
+            // setup mocks
+
+            taskRepo.Setup(repo => repo.GetByIdAsync(taskId)).ReturnsAsync(task);
+
+            projectRepo.Setup(repo => repo.GetByIdAsync(task.ProjectId)).ReturnsAsync(project);
+
+            memberRepo.Setup(repo => repo.IsExistAsync(It.IsAny<Guid>(), It.IsAny<Guid>())).ReturnsAsync(true);
+
+            var taskService = new TaskService(
+                taskRepo.Object,
+                userRepo.Object,
+                projectRepo.Object,
+                memberRepo.Object,
+                unitOfWork.Object
+            );
+
+            //
+            //
+            // act
+            await taskService.AssignTaskAsync(currentUserId, taskId, userId);
+
+
+            //
+            // Assert
+            taskRepo.Verify(repo => repo.Update(task), Times.Once);
+            unitOfWork.Verify(repo => repo.SaveChangesAsync(), Times.Once);
+
+            Assert.Equal(userId, task.AssignedToUserId);
+        }
+    }
+}
